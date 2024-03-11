8294a7be991492a8b2896f6a79fdcdba2590e38c
Hajkhlifa <abdelalihajkhlifa@gmail.com>
2024-03-11 16:25:24 +0000

    config.php

--- config.php
+++ config.php
@@ -3,5 +3,13 @@
    	define('DB_USERNAME', 'root');
    	define('DB_PASSWORD', '');
    	define('DB_DATABASE', 'studentportal');
-   	$connection = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   	$dsn = "host:".DB_SERVER."dbname=".DB_DATABASE.";
+	try {
+		$connection = new PDO($dsn,DB_USERNAME);
+	}catch(PDOExeption $e){
+		$error = $e->getMessage();
+	}
 ?>
+
+
+
