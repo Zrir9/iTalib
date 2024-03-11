914bcb174c8f46b29b9c92e4fdcfa5ad31b4da2b
Hajkhlifa <abdelalihajkhlifa@gmail.com>
2024-03-11 17:32:45 +0000

    config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
    	define('DB_USERNAME', 'root');
    	define('DB_PASSWORD', '');
    	define('DB_DATABASE', 'studentportal');
-   	$dsn = "host:".DB_SERVER."dbname=".DB_DATABASE.";
+   	$dsn = "host:".DB_SERVER."dbname=".DB_DATABASE."";
 	try {
 		$connection = new PDO($dsn,DB_USERNAME);
-	}catch(PDOExeption $e){
+	}catch(PDOException $e){
 		$error = $e->getMessage();
 	}
 ?>
