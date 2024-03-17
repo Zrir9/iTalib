745e41ace56070b0a911eb73081c2a6d59b25117
Hajkhlifa <abdelalihajkhlifa@gmail.com>
2024-03-17 15:00:19 +0000

    nouvell version

--- config.php
+++ config.php
@@ -1,15 +1,12 @@
 <?php
-   	define('DB_SERVER', 'localhost');
-   	define('DB_USERNAME', 'root');
-   	define('DB_PASSWORD', '');
-   	define('DB_DATABASE', 'studentportal');
-   	$dsn = "host:".DB_SERVER."dbname=".DB_DATABASE."";
-	try {
-		$connection = new PDO($dsn,DB_USERNAME);
-	}catch(PDOException $e){
-		$error = $e->getMessage();
-	}
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'studentportal');
+$dsn = "mysql:host=" . DB_SERVER . ";dbname=" . DB_DATABASE;
+try {
+    $db = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
+} catch(PDOException $e) {
+    echo "Connection error: " . $e->getMessage();
+}
 ?>
-
-
-
