3bb72d8f6b6c2d42445dc1fdfd26e25042a87505
Omar <omarfotouh89@gmail.com>
2024-03-17 15:03:02 +0000

    config.php modified

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
    	define('DB_DATABASE', 'studentportal');
    	$dsn = "host:".DB_SERVER."dbname=".DB_DATABASE."";
 	try {
-		$connection = new PDO($dsn,DB_USERNAME);
+		$db = new PDO($dsn,DB_USERNAME);
 	}catch(PDOException $e){
 		$error = $e->getMessage();
 	}
